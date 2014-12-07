88276bfa511ecccbb8324516b3f5a6e8cd1667fa
jozwright <jozwright@gmail.com>
2014-12-06 18:24:41 -0800

    create database 'csv_importer'

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'sinatra/flash'
 require 'sinatra/activerecord'
 
 Dir.glob('./lib/*.rb').each { |f| require f }
+set :database, 'postgres://localhost/csv_importer'
 
 get '/' do
   erb :index
