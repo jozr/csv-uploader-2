6a5c147f712a02a74f09e6a0aa62a7ca8d5010cb
jozwright <jozwright@gmail.com>
2014-12-07 23:41:28 -0800

    WIP

--- app.rb
+++ app.rb
@@ -8,3 +8,13 @@ set :database, 'postgres://localhost/csv_importer'
 get '/' do
   erb :index
 end
+
+post '/' do
+  file = params['upload']
+  File.open('uploads/' + file[:filename], "w") do |f|
+    f.write(file[:tempfile].read)
+  end
+  file_name = File.absolute_path('uploads/' + file[:filename]) 
+  read_file = CSV.read(file_name)
+  return "file: #{read_file}"
+end
--- lib/models.rb
+++ lib/models.rb
@@ -1,3 +1,8 @@
+require 'csv'
+
+class Upload
+end
+
 class Artist < ActiveRecord::Base
   validates :name, :presence => true
   validates_length_of :name, maximum: 95
